42aa09e23a4a3b3f0b8b28d057bf453cb0597201
mely_casta_eda <melycastp@gmail.com>
2020-05-05 22:20:43 -0500

    Updates

---
+++ precache-manifest.fa73f086bbcd40fa3d9ab04e0ea99021.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "040f6ebe2fbb3628b33f876195a4a13b",
+    "url": "/monsters-react/index.html"
+  },
+  {
+    "revision": "f11f4c82a5155240f618",
+    "url": "/monsters-react/static/css/main.77064991.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-react/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-react/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f11f4c82a5155240f618",
+    "url": "/monsters-react/static/js/main.352a7db9.chunk.js"
+  },
+  {
+    "revision": "bb10d4d4a0632a4e8caf",
+    "url": "/monsters-react/static/js/runtime-main.2624874f.js"
+  }
+]);
